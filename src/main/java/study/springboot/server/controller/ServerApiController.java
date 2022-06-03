8855a06caae843873cc9e4a32a3972f62a317d32
hclee <heechul4296@gmail.com>
2022-06-03 14:35:04 +0900

    네이버 검색 api 사용

--- src/main/java/study/springboot/server/controller/ServerApiController.java
+++ src/main/java/study/springboot/server/controller/ServerApiController.java
@@ -2,14 +2,53 @@ package study.springboot.server.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 import study.springboot.server.dto.Request;
 import study.springboot.server.dto.User;
+
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/server")
 public class ServerApiController {
+
+    /**
+     * https://openapi.naver.com
+     * /v1/search/local.json
+     * ?query=\xea\xb0\x88\xeb\xb9\x84\x0a&display=10&start=1&sort=random
+     */
+    @GetMapping("/naver")
+    public String naver() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("/v1/search/local.json")
+                .queryParam("query", "중국집")
+                .queryParam("display", 10)
+                .queryParam("start", 1)
+                .queryParam("sort", "random")
+                .encode(Charset.forName("UTF-8"))
+                .build()
+                .toUri();
+        System.out.println("uri = " + uri);
+
+        RestTemplate template = new RestTemplate();
+        RequestEntity<Void> request = RequestEntity
+                .get(uri)
+                .header("X-Naver-Client-Id", "FJ7Ood2nGx2rvNB0AsQX")
+                .header("X-Naver-Client-Secret", "MYsgp0gGQK")
+                .build();
+
+        ResponseEntity<String> response = template.exchange(request, String.class);
+        return response.getBody();
+    }
 
     @GetMapping(value = "/hello")
     public User hello(@RequestParam("name") String name, @RequestParam("age") int age) {
